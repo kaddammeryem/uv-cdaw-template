8c7a2d250d8ba129bbcd3e2f8f6b57614db0996e
Meryem Kaddam <kaddammeryem@gmail.com>
2021-11-11 19:48:46 +0100

    accueil deco part2

---
+++ public/jalon1/testapi.php
@@ -0,0 +1,39 @@
+
+<html>
+<?php
+$url = 'https://imdb-api.com/en/API/Top250Movies/k_fuxbwlin';
+
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'https' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'GET',
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url);
+$obj=json_decode($result,true,512,JSON_OBJECT_AS_ARRAY);
+if ($result === FALSE) { /* Handle error */ }
+?>
+<table table style="width:100%"; table class="data-table">
+   <tr>
+       <th>title</th>
+       <th>fullTitle</th>
+       <th>year</th>
+   </tr>
+
+   
+       <?php for($i=0;$i<count($obj["items"]);$i=$i+1) {?>
+        <tr>
+       <td><?php echo $obj["items"][$i]["title"]; ?></td>
+       <td><?php echo $obj["items"][$i]["fullTitle"]; ?></td>
+       <td><?php echo $obj["items"][$i]["year"]."\n"; ?></td>
+       </tr>
+
+<?php } ?>
+       
+
+</table>
+
+</html>
